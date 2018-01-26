b7685f2a7532dc3188c8ae52b34ba434def073fa
skiff <skiffc@gmail.com>
2018-01-26 14:03:33 +0800

    For CPU only solution

--- lib/model/nms_wrapper.py
+++ lib/model/nms_wrapper.py
@@ -9,10 +9,10 @@ from __future__ import division
 from __future__ import print_function
 
 from model.config import cfg
-from nms.gpu_nms import gpu_nms
+#from nms.gpu_nms import gpu_nms
 from nms.cpu_nms import cpu_nms
 
-def nms(dets, thresh, force_cpu=False):
+def nms(dets, thresh, force_cpu=True):
   """Dispatch to either CPU or GPU NMS implementations."""
 
   if dets.shape[0] == 0:
